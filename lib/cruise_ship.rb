bee355b6524b2dbb31197fec5ea0bfc9a30b6e60
Sanny Do <sanny.do94@gmail.com>
2020-08-24 05:53:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,8 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
+    if suite == :suite_a && name.start_with? ("A")
       puts "You are the winner, #{name}!"
     end
   end
