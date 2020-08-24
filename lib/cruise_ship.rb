7f32f90ac0e8f80234af4d6adee99a8af1fa51e9
Sanny Do <sanny.do94@gmail.com>
2020-08-24 06:59:28 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -13,12 +13,6 @@ def select_winner(passengers)
  if suite == :suite_a && name.start_with?("A")
    winner = name
   end
-end
+ end
 winner
 end
-   
-   passengers.has_key?("suite_a") && passengers.select { |value| value.start_with?('A') }
-   puts "Amanda Presley is the winner!"
- end
-end
-
