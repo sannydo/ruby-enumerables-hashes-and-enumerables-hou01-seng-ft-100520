76143732fdef1ee5fa19af222a78bbd17c18eab3
Sanny Do <sanny.do94@gmail.com>
2020-08-24 05:50:41 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,6 +11,8 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      puts "You are the winner!"
+      winner = name
   end
 end
+
+winner
