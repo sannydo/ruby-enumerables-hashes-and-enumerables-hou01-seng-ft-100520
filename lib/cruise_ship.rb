e0a1259a9b94d67ac90b2e3d16fdf66e17eb87ce
Sanny Do <sanny.do94@gmail.com>
2020-08-24 05:35:36 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,6 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
-    puts "Hello #{key}! You are now #{value} years old!"
+    puts "Hello #{key}! You are #{value}!"
   end
 end
